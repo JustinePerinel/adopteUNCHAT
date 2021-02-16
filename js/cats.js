4ad10aa8bb2f28f95ece00f537b4ffa7eb2fcbb3
Justine Périnel <justine.perinel@free.Fr>
2021-02-16 09:16:43 +0100

    maj

--- js/cats.js
+++ js/cats.js
@@ -4,18 +4,10 @@
 
 var formulaire = document.getElementById('form1');
 var textarea = document.getElementById('textarea');
-// if (isNaN(textarea)){
     document.querySelector('#btnForm1').addEventListener('click', function(){
         formulaire.innerHTML = "Votre demande a bien été envoyée ! Nous vous répondrons dans les meilleurs délais.";
         formulaire.classList.add('alert', 'alert-success');
     })
-// }else {
-//     document.querySelector('#btnForm1').addEventListener('click', function(){
-//         formulaire.innerHTML = "Votre demande n'a pas été prise en compte ! Vous devez remplir le champ de formulaire.";
-//         formulaire.classList.add('alert', 'alert-danger');
-//     })
-// }
-// POUR FAIRE CHANGER LA COULEUR DU CHAT
 
 var teteDeChat = document.querySelector('#teteDeChat').addEventListener('mouseover', function(){
     this.style.backgroundColor = "#061339";
