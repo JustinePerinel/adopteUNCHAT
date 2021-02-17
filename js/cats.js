7739e6451d719f42b34d884e1cc9916bdd5097d3
Justine Périnel <justine.perinel@free.fr>
2021-02-17 21:26:27 +0100

    maj

--- js/cats.js
+++ js/cats.js
@@ -4,10 +4,13 @@
 
 var formulaire = document.getElementById('form1');
 var textarea = document.getElementById('textarea');
-    document.querySelector('#btnForm1').addEventListener('click', function(){
-        formulaire.innerHTML = "Votre demande a bien été envoyée ! Nous vous répondrons dans les meilleurs délais.";
-        formulaire.classList.add('alert', 'alert-success');
-    })
+document.querySelector('#btnForm1').addEventListener('click', function(){
+    formulaire.innerHTML = "Votre demande a bien été envoyée ! Nous vous répondrons dans les meilleurs délais.";
+    formulaire.classList.add('alert', 'alert-success');
+})
+
+
+// POUR CHANGER LA COULEUR DE LA TETE DE CHAT SUR LE HOVER
 
 var teteDeChat = document.querySelector('#teteDeChat').addEventListener('mouseover', function(){
     this.style.backgroundColor = "#061339";
